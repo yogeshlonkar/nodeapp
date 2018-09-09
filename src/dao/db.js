6f9a69cc17c16513cd9302f3946fa48885154327
Yogesh Lonkar <lonkar.yogeshr@gmail.com>
2018-09-09 19:44:04 +0530

    replica-set support for monogdb cluster

--- src/dao/db.js
+++ src/dao/db.js
@@ -6,7 +6,8 @@ module.exports = (async () => {
   // Use connect method to connect to the server
   console.info('connection string =>', config.connectionString);
   const client = await MongoClient.connect(config.connectionString, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    replicaSet: process.env.DB_REPLSET || undefined
   });
   console.info('Connected successfully to server');
   return {
